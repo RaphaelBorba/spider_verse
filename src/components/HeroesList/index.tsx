739f9ca77eef84b173b8f8a617b0b31d8567e173
Raphael Borba <raphaelbsx@gmail.com>
2023-11-20 21:50:11 -0300

    feat: css

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,3 +1,5 @@
+import HeroPicture from "../heroPicture";
+
 import styles from "./herosList.module.scss";
 
 import { spidermanFont } from "@/fonts";
@@ -13,6 +15,17 @@ export default function HeroesList({ heroes }: IProps) {
       <h1 className={`${spidermanFont.className} ${styles.title}`}>
         Personagens
       </h1>
+
+      <section className={styles.heroes}>
+        {heroes.map((hero) => (
+          <div
+            key={hero.id}
+            className={`${styles.imageContainer} ${styles[hero.id]}`}
+          >
+            <HeroPicture hero={hero} />
+          </div>
+        ))}
+      </section>
     </>
   );
 }
