ff6f965a0858abafbea07abbfa62afc09e00e5e2
Raphael Borba <raphaelbsx@gmail.com>
2023-11-20 22:16:14 -0300

    feat: add framer motion libary

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+
 import HeroPicture from "../heroPicture";
 
 import styles from "./herosList.module.scss";
@@ -12,20 +15,33 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+      <motion.h1
+        className={`${spidermanFont.className} ${styles.title}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 1 }}
+      >
         Personagens
-      </h1>
+      </motion.h1>
 
-      <section className={styles.heroes}>
+      <motion.section
+        className={styles.heroes}
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
         {heroes.map((hero) => (
-          <div
+          <motion.div
             key={hero.id}
             className={`${styles.imageContainer} ${styles[hero.id]}`}
+            whileHover={{ scale: 1.3 }}
+            transition={{ duration: 0.5 }}
+            whileTap={{ scale: 0.8 }}
           >
             <HeroPicture hero={hero} />
-          </div>
+          </motion.div>
         ))}
-      </section>
+      </motion.section>
     </>
   );
 }
