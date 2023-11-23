38fd640b22ea8a682abf4f50cbc9db7f45c698ba
Raphael Borba <raphaelbsx@gmail.com>
2023-11-23 12:28:50 -0300

    add: change env places

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://64a54c6300c3559aa9bf7245.mockapi.io/api",
-    DOMAIN_ORIGIN: "http:// 127.0.0.1:3000",
+    API_URL: process.env.API_URL,
+    DOMAIN_ORIGIN: process.env.DOMAIN_ORIGIN,
   },
 };
 
