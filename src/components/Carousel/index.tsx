e15596df73889db92fd88c63d036240c4ed2e7be
Raphael Borba <raphaelbsx@gmail.com>
2023-11-22 21:24:51 -0300

    add: animetion carosel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 import HeroDetails from "../HeroDetails";
 import HeroPicture from "../heroPicture";
@@ -32,6 +33,10 @@ export default function Carousel({ heroes, activeId }: IProps) {
     setVisibleItem(visibleItems);
   }, [heroes, activeIndex]);
 
+  const handleChangeActiveIndex = (newDirection: number) => {
+    setActiveIndex((prevActiveIndex) => prevActiveIndex + newDirection);
+  };
+
   if (!visibleItems) {
     return null;
   }
@@ -39,12 +44,24 @@ export default function Carousel({ heroes, activeId }: IProps) {
   return (
     <div className={styles.container}>
       <div className={styles.carousel}>
-        <div className={styles.wrapper}>
-          {visibleItems.map((item) => (
-            <div key={item.id} className={styles.hero}>
-              <HeroPicture hero={item} />
-            </div>
-          ))}
+        <div
+          className={styles.wrapper}
+          onClick={() => handleChangeActiveIndex(1)}
+        >
+          <AnimatePresence mode="popLayout">
+            {visibleItems.map((item, position) => (
+              <motion.div
+                key={item.id}
+                className={styles.hero}
+                initial={{ x: -1500, scale: 0.75 }}
+                exit={{ x: 0, opacity: 0, scale: 1, left: "-20%" }}
+                animate={{ x: 0, ...getItemStyles(position) }}
+                transition={{ duration: 0.8 }}
+              >
+                <HeroPicture hero={item} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
         </div>
       </div>
 
@@ -54,3 +71,37 @@ export default function Carousel({ heroes, activeId }: IProps) {
     </div>
   );
 }
+
+enum enPosition {
+  FRONT = 0,
+  MIDDLE = 1,
+  BACK = 2,
+}
+
+const getItemStyles = (position: enPosition) => {
+  if (position === enPosition.FRONT) {
+    return {
+      zIndex: 3,
+      filter: "blur(10px)",
+      scale: 1.2,
+    };
+  }
+
+  if (position === enPosition.MIDDLE) {
+    return {
+      zIndex: 2,
+      left: 300,
+      scale: 0.8,
+      top: "-10%",
+    };
+  }
+
+  return {
+    zIndex: 1,
+    filter: "blur(10px)",
+    left: 160,
+    top: "-20%",
+    scale: 0.6,
+    opacity: 0.8,
+  };
+};
