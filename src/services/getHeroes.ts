d82300335eb0cf5b2cd1b249d2f7dd4778287dce
Raphael Borba <raphaelbsx@gmail.com>
2023-11-23 12:15:35 -0300

    .

--- src/services/getHeroes.ts
+++ src/services/getHeroes.ts
@@ -1,7 +1,9 @@
 import { IHeroData } from "@/interfaces/heroes";
 
 export async function getHeroesData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`, {
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     throw new Error("Failed to request heroes list!");
