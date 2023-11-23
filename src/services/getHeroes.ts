7cf047c56df568f1e4a9bba3962e6cbb673eb68a
Raphael Borba <raphaelbsx@gmail.com>
2023-11-23 13:43:09 -0300

    fix

--- src/services/getHeroes.ts
+++ src/services/getHeroes.ts
@@ -1,6 +1,8 @@
 import { IHeroData } from "@/interfaces/heroes";
 
-export async function getHeroesData(): Promise<{ data: IHeroData[] }> {
+export async function getHeroesData(): Promise<{ data: IHeroData[] } | null> {
+  if (!process.env.NEXT_PUBLIC_BASE_API_URL) return null;
+
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/heroes`);
 
   if (!res.ok) {
